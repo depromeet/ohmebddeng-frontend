015e1c0820e9d35328ed2fc57d2d6ca2ac9a03ea
SeongYongLee <seongyong.dev@gmail.com>
2021-10-03 01:44:16 +0900

    feat: storybook + emotion Setting

---
+++ .storybook/main.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const fs = require('fs');
+const { merge } = require('webpack-merge');
+
+function getPackageDir(filepath) {
+  let currDir = path.dirname(require.resolve(filepath));
+  while (true) {
+    if (fs.existsSync(path.join(currDir, 'package.json'))) {
+      return currDir;
+    }
+    const { dir, root } = path.parse(currDir);
+    if (dir === root) {
+      throw new Error(
+        `Could not find package.json in the parent directories starting from ${filepath}.`
+      );
+    }
+    currDir = dir;
+  }
+}
+
+module.exports = {
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-postcss',
+  ],
+  webpackFinal: async (config) => {
+    return merge(config, {
+      resolve: {
+        alias: {
+          '@emotion/core': getPackageDir('@emotion/react'),
+          '@emotion/styled': getPackageDir('@emotion/styled'),
+          'emotion-theming': getPackageDir('@emotion/react'),
+        },
+      },
+    });
+  },
+};
