473c39fd6fb67a8aadf567d5435e29beed06bdad
jjunyjjuny <jjuny546@gmail.com>
2021-10-03 03:33:25 +0900

    init: Jest + Testing-library setting

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  moduleNameMapper: {
+    '\\.(scss|sass|css)$': 'identity-obj-proxy',
+    '@/(.*)': '<rootDir>/src/$1',
+  },
+  moduleDirectories: ['node_modules', 'src'],
+};
