4fcb9b6a4329d6c70f6d11b4d27d3ee59662505d
moonheekim0118 <hellomooneekim@gmail.com>
2021-10-07 01:00:13 +0900

    feat: TasteTagInput 스타일링 구현

---
+++ src/components/Common/TasteTagInput/index.stories.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { TASTE } from '@/types';
+import TasteTagInput from '.';
+
+export default {
+  title: 'Common/TasteTagInput',
+  component: TasteTagInput,
+} as ComponentMeta<typeof TasteTagInput>;
+
+const Template: ComponentStory<typeof TasteTagInput> = (args) => (
+  <TasteTagInput {...args} />
+);
+export const Unchecked = Template.bind({});
+
+Unchecked.args = {
+  name: TASTE.끝맛이매워요,
+  checked: false,
+};
+
+export const Checked = Template.bind({});
+
+Checked.args = {
+  name: TASTE.끝맛이매워요,
+  checked: true,
+};
--- src/components/Common/TasteTagInput/index.tsx
+++ src/components/Common/TasteTagInput/index.tsx
@@ -1,17 +1,32 @@
 import React from 'react';
 import { TASTE } from '@/types';
+import styled from '@emotion/styled';
 interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
   name: TASTE;
 }
 
 const TasteTagInput = (props: Props) => {
-  const { name, type, ...rest } = props;
+  const { name, type, checked, ...rest } = props;
   return (
-    <div>
-      <input type={type ?? 'checkbox'} name={name} value={name} {...rest} />
-      <label htmlFor={name}>#{name}</label>
-    </div>
+    <Container checked={checked}>
+      <Input type={type ?? 'checkbox'} name={name} value={name} {...rest} />#{' '}
+      {name}
+    </Container>
   );
 };
+
+const Container = styled.label<{ checked?: boolean }>`
+  width: 100%;
+  border-radius: 24px;
+  background-color: ${({ checked }) => (checked ? '#FF5252' : '#3A3A3C')};
+  color: ${({ checked }) => (checked ? '#F5F5F5' : '#8E8E93')};
+  padding: 12px 18px 11px;
+  text-align: center;
+  cursor: pointer;
+`;
+
+const Input = styled.input`
+  display: none;
+`;
 
 export default TasteTagInput;
