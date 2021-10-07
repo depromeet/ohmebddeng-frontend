832bfbb7f5129553a18c98e501a87ecb6766689c
moonheekim0118 <hellomooneekim@gmail.com>
2021-10-07 14:24:37 +0900

    feat: font 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { initMSW } from '@/lib/msw';
 import { GlobalStyle } from '@/styles';
 import type { AppProps } from 'next/app';
 import Layout from '@/components/Layout';
+import Head from 'next/head';
 
 initMSW();
 
@@ -9,6 +10,26 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <GlobalStyle />
+      <Head>
+        <link
+          rel="preload"
+          href="assets/fonts/SB-Aggro-OTF-B.otf"
+          as="font"
+          crossOrigin=""
+        />
+        <link
+          rel="preload"
+          href="assets/fonts/SB-Aggro-OTF-M.otf"
+          as="font"
+          crossOrigin=""
+        />
+        <link
+          rel="preload"
+          href="assets/fonts/SB-Aggro-OTF-L.otf"
+          as="font"
+          crossOrigin=""
+        />
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
