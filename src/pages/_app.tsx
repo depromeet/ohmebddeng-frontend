7daa9236bd5f7c3f8f4ca8df237e278b727c7b58
eunsukimme <eunsu.dev@gmail.com>
2021-10-09 00:35:27 +0900

    feat: add theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import { initMSW } from '@/lib/msw';
 import { GlobalStyle } from '@/styles';
+import theme from '@/styles/theme';
+import { ThemeProvider } from '@emotion/react';
 import type { AppProps } from 'next/app';
 
 initMSW();
@@ -8,7 +10,9 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
     </>
   );
 }
