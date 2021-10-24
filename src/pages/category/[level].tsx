31410e33e16a890f5a9a6e18a7ccb0a404fb9c71
eunsukimme <eunsu.dev@gmail.com>
2021-10-24 17:44:04 +0900

    feat: add food list

--- src/pages/category/[level].tsx
+++ src/pages/category/[level].tsx
@@ -1,6 +1,7 @@
 import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import type { NextPage } from 'next';
+import Image from 'next/image';
 import { useRouter } from 'next/router';
 import { Header } from '@/components/Common';
 import { TASTE_LEVEL } from '@/types';
@@ -8,6 +9,7 @@ import { TASTE_LEVEL } from '@/types';
 const CategoryByTaste: NextPage = () => {
   const router = useRouter();
   const { level } = router.query;
+
   return (
     <div>
       <Header type="center">오늘 뭐가 땡겨?</Header>
@@ -19,6 +21,26 @@ const CategoryByTaste: NextPage = () => {
             </Tab>
           ))}
         </Tabs>
+        <Lists>
+          {data.map((food) => (
+            <FoodItem key={food.id}>
+              <Image
+                src={food.image_url}
+                alt={food.name}
+                width={52}
+                height={42}
+              />
+              <div
+                css={css`
+                  margin-left: 26px;
+                `}
+              >
+                <Name>{food.name}</Name>
+                <Info>{food.description}</Info>
+              </div>
+            </FoodItem>
+          ))}
+        </Lists>
       </Wrapper>
     </div>
   );
@@ -45,5 +67,55 @@ const Tab = styled.button<{ active?: boolean }>`
       border-bottom: 2px solid ${theme.colors.red};
     `}
 `;
+const Lists = styled.ul`
+  margin: 32px 0 0 0;
+  padding: 0 0 0 14px;
+  display: flex;
+  flex-direction: column;
+
+  & > * + * {
+    margin-top: 44px;
+  }
+`;
+const FoodItem = styled.div`
+  display: flex;
+`;
+const Name = styled.div`
+  margin-bottom: 8px;
+  font-weight: bold;
+  font-size: 15px;
+`;
+const Info = styled.div`
+  font-size: 13px;
+  color: #8e8e93;
+`;
 
 export default CategoryByTaste;
+
+// TODO: API로부터 데이터 불러오기
+const data = [
+  {
+    id: 1,
+    name: '엽기떡볶이 1단계',
+    image_url: '/assets/FoodReview/0.svg',
+    description: '#칼칼한 #매콤달콤',
+  },
+  {
+    id: 2,
+    name: '엽기떡볶이 1단계',
+    image_url: '/assets/FoodReview/0.svg',
+    description: '#칼칼한 #매콤달콤',
+  },
+  {
+    id: 3,
+    name: '엽기떡볶이 1단계',
+    image_url: '/assets/FoodReview/0.svg',
+    description: '#칼칼한 #매콤달콤',
+  },
+  {
+    id: 4,
+    name: '엽기떡볶이 1단계',
+    image_url: '/assets/FoodReview/0.svg',
+    description: '#칼칼한 #매콤달콤',
+  },
+];
