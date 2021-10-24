1c2d7ef8a463fea94672aaddb3e577e0ed0b1698
eunsukimme <eunsu.dev@gmail.com>
2021-10-24 17:13:18 +0900

    feat: add basic category page

--- src/pages/category/[level].tsx
+++ src/pages/category/[level].tsx
@@ -1,10 +1,49 @@
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
 import type { NextPage } from 'next';
 import { useRouter } from 'next/router';
+import { Header } from '@/components/Common';
+import { TASTE_LEVEL } from '@/types';
 
 const CategoryByTaste: NextPage = () => {
   const router = useRouter();
   const { level } = router.query;
-  return <div>{level} 카테고리 페이지</div>;
+  return (
+    <div>
+      <Header type="center">오늘 뭐가 땡겨?</Header>
+      <Wrapper>
+        <Tabs>
+          {Object.values(TASTE_LEVEL).map((_level) => (
+            <Tab key={_level} active={_level === level}>
+              {_level}
+            </Tab>
+          ))}
+        </Tabs>
+      </Wrapper>
+    </div>
+  );
 };
+
+const Wrapper = styled.div`
+  padding: 0 16px;
+`;
+const Tabs = styled.nav`
+  display: flex;
+`;
+const Tab = styled.button<{ active?: boolean }>`
+  padding: 13px 0;
+  flex: 1;
+  color: ${({ theme }) => theme.colors.grey20};
+  background-color: transparent;
+  border: none;
+  outline: none;
+
+  ${({ theme, active }) =>
+    active &&
+    css`
+      color: ${theme.colors.white};
+      border-bottom: 2px solid ${theme.colors.red};
+    `}
+`;
 
 export default CategoryByTaste;
