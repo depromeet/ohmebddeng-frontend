a8316e533e7e0295187a06d45bf65b601a67e46c
SeongYongLee <seongyong.dev@gmail.com>
2021-10-28 00:58:18 +0900

    feat: reviewWrite 페이지 작업 및 공통 컴포넌트로 변경

--- src/pages/reviewWrite.tsx
+++ src/pages/reviewWrite.tsx
@@ -1,25 +1,28 @@
 import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import type { NextPage } from 'next';
-import Image from 'next/image';
-import React, { useState } from 'react';
-import { Header } from '@/components/Common';
+import { useRouter } from 'next/router';
+import React, { useCallback, useState } from 'react';
+import { Header, SpicyLevelSection } from '@/components/Common';
+import FoodOverview from '@/components/Common/FoodOverview';
 import Button from '@/components/Input/Button';
-import { SpicyLevelForm, TasteForm } from '@/components/Review';
-import theme from '@/styles/theme';
+import { TasteForm } from '@/components/Review';
+import { ROUTES } from '@/constants';
 import { Food, LEVEL, ReviewState, TASTE } from '@/types';
 
 const ReviewWrite: NextPage<Food> = ({
   image_url = '/assets/FoodReview/0.svg',
   name = '진라면 순한맛',
-  id = 1,
+  id = '1',
 }) => {
+  const router = useRouter();
   const [review, setReview] = useState<ReviewState>({ taste: new Set() });
+  const food = { image_url, name, id } as Food;
 
   const handleCheckLevel =
     () => (event: React.ChangeEvent<HTMLInputElement>) => {
       const level = (event.target as HTMLInputElement).value as LEVEL;
-      setReview({ level });
+      setReview({ ...review, level });
     };
   const handleCheckTaste =
     () => (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -28,81 +31,46 @@ const ReviewWrite: NextPage<Food> = ({
       taste.has(targetTaste)
         ? taste.delete(targetTaste)
         : taste.add(targetTaste);
-      setReview({ taste });
+      setReview({ ...review, taste });
     };
+  const handleClick = useCallback(() => {
+    router.push(ROUTES.MAIN);
+  }, [router]);
 
   return (
-    <div>
-      <div>
-        <Header type="center">리뷰 작성하기</Header>
+    <>
+      <Header type="center">
+        <span>리뷰 작성하기</span>
+      </Header>
 
-        <section
-          css={css`
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            padding: 62px 54px;
-          `}
-        >
-          <Image src={image_url} alt={name} width={124} height={170} />
-          <p
-            css={css`
-              font-family: SBAggroB;
-              font-weight: normal;
-              padding-top: 18px;
-              font-size: 32px;
-              line-height: 140%;
-              color: ${theme.colors.grey0};
-            `}
-          >
-            {name}
-          </p>
-        </section>
+      <FoodOverview {...food} />
+      <SpicyLevelSection
+        disabled={false}
+        level={review?.level}
+        onChange={handleCheckLevel()}
+      />
+      <TasteSection>
+        <h3>어땠나요?</h3>
+        <TasteForm taste={review?.taste} onChange={handleCheckTaste()} />
+      </TasteSection>
 
-        <section
-          css={css`
-            margin: 0 16px;
-            padding: 24px 26px;
-            background-color: rgba(255, 255, 255, 0.1);
-            border-radius: 14px;
-          `}
-        >
-          <h3
-            css={css`
-              margin-bottom: 26px;
-              font-weight: 800;
-              font-size: 17px;
-              color: ${theme.colors.white};
-            `}
-          >
-            얼마나 맵게 느껴지나요?
-          </h3>
-          <SpicyLevelForm level={review?.level} onChange={handleCheckLevel()} />
-        </section>
-
-        <TasteSection>
-          <h3>어땠나요?</h3>
-          <TasteForm taste={review?.taste} onChange={handleCheckTaste()} />
-        </TasteSection>
-
-        <Button
-          buttonType="contained"
-          color="red"
-          rounded={false}
-          css={css`
-            width: 100%;
-          `}
-          onClick={() => alert('음식상세 페이지로 이동하겠죠...?')}
-        >
-          등록
-        </Button>
-      </div>
-    </div>
+      <Button
+        buttonType="contained"
+        color="red"
+        rounded={false}
+        css={css`
+          width: 100%;
+        `}
+        onClick={() => handleClick()}
+      >
+        등록
+      </Button>
+    </>
   );
 };
 
 const TasteSection = styled.div`
-  padding: 24px 26px 48px;
+  padding: 38px 26px 48px;
   text-align: left;
 `;
 
