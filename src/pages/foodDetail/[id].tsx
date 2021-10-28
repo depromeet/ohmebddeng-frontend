f8164b10356a83d8332618370f0049eaf6ef207c
moonheekim0118 <hellomooneekim@gmail.com>
2021-10-28 12:28:55 +0900

    feat: foodOverview props 추가 및 next/image domain 설정

--- src/pages/foodDetail/[id].tsx
+++ src/pages/foodDetail/[id].tsx
@@ -1,5 +1,7 @@
+import styled from '@emotion/styled';
 import type { NextPage } from 'next';
 import { useRouter } from 'next/router';
+import { Header, FoodOverview } from '@/components/Common';
 import { SpicyEvaluation } from '@/components/FoodDetail';
 import { TASTE_LEVEL } from '@/types';
 
@@ -14,10 +16,29 @@ const FoodDetail: NextPage = () => {
   const router = useRouter();
   const { id } = router.query;
   return (
-    <div>
-      <SpicyEvaluation testData={data} totalCount={17} />
-    </div>
+    <>
+      <Header type="center">
+        <span>불닭볶음면 까르보나라맛</span>
+      </Header>
+      <Container>
+        <FoodOverview
+          image_url={`https://cdn.ohmebddeng.kr/foods/ramyeon.png`}
+          id="불닭볶음면"
+          name="불닭볶음면"
+          nameVisable={false}
+        />
+        {/** TODO: FoodOverview 컴포넌트 삽입 */}
+        <SpicyEvaluation testData={data} totalCount={17} />
+      </Container>
+    </>
   );
 };
+
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 export default FoodDetail;
